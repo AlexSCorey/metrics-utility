f2b5a83a62c833a8ddd3d985968228d3ef4d9bb4
Alex Corey <acorey@redhat.com>
2025-06-11 08:14:14 -0400

    AAP-45505 - Asserts that exceptions for build and gather appear with the proper messaging. (#128)
    * Adds validation for build report and gather date time frame arguments.
    
    Refactor and code clean up.
    
    Refactors validation to handle multiple valid date formats.
    
    Fine tunes the date param validation function to be more robust, and removes some code that is now dead.
    
    * Refactoring

--- metrics_utility/management/commands/gather_automation_controller_billing_data.py
+++ metrics_utility/management/commands/gather_automation_controller_billing_data.py
@@ -21,11 +21,8 @@ from metrics_utility.management.validation import (
     handle_not_crc,
     handle_not_s3,
     handle_s3_ship_target,
+    handle_validate_date_param,
 )
-
-
-help_since = 'Start date for collection including (e.g. --since=2023-12-20), a number of days ago (--since=5d), or a number of months (--since=2m).'
-help_until = 'End date for collection including (e.g. --until=2023-12-21), a number of days ago (--until=5d), or a number of months (--until=2m).'
 
 
 class Command(BaseCommand):
@@ -33,7 +30,20 @@ class Command(BaseCommand):
     Gather Automation Controller billing data
     """
 
-    help = 'Gather Automation Controller billing data'
+    def __init__(self):
+        super().__init__()
+        self.help = {
+            'since': (
+                'Start date for collection including (e.g. --since=2023-12-20), a number of days ago (--since=5d), '
+                'or a number of months (--since=2m).'
+            ),
+            'until': (
+                'End date for collection including (e.g. --until=2023-12-21), a number of days ago (--until=5d), or a number of months (--until=2m).'
+            ),
+            'time_frame_extra_params': (
+                'Missing required parameter --until, or --since. Metrics utility requires a value for at least one of the following: since, until.'
+            ),
+        }
 
     def add_arguments(self, parser):
         parser.add_argument('--dry-run', dest='dry-run', action='store_true', help='Gather billing metrics without shipping.')
@@ -43,13 +53,13 @@ class Command(BaseCommand):
             '--since',
             dest='since',
             action='store',
-            help=help_since,
+            help=self.help.get('since'),
         )
         parser.add_argument(
             '--until',
             dest='until',
             action='store',
-            help=help_until,
+            help=self.help.get('until'),
         )
 
     def init_logging(self):
@@ -64,7 +74,7 @@ class Command(BaseCommand):
         try:
             self._handle(self, *args, **options)
             exit(0)
-        except (BadShipTarget, MissingRequiredEnvVar, BadRequiredEnvVar, FailedToUploadPayload) as e:
+        except (BadShipTarget, MissingRequiredEnvVar, BadRequiredEnvVar, FailedToUploadPayload, UnparsableParameter) as e:
             self.logger.error(e.name)
             exit(1)
         except Exception as e:
@@ -73,6 +83,10 @@ class Command(BaseCommand):
 
     def _handle(self, *args, **options):
         self.init_logging()
+
+        handle_validate_date_param(options.get('since', None), self.help.get('since'), 'gather')
+        handle_validate_date_param(options.get('until', None), self.help.get('until'), 'gather')
+
         opt_ship = options.get('ship')
         opt_dry_run = options.get('dry-run')
         opt_since = options.get('since') or None
@@ -119,13 +133,7 @@ class Command(BaseCommand):
     def _handle_datelike(self, value, help=''):
         if not value:
             return None
-
-        if value.isdigit():
-            raise UnparsableParameter(f'Bare numbers are not valid ({help})')
-
-        # Process ret argument
-        ret = None
-
+        # # Process ret argument
         if value.endswith('d'):
             days_ago = int(value[0:-1])
             ret = (datetime.datetime.now() - datetime.timedelta(days=days_ago - 1)).replace(hour=0, minute=0, second=0, microsecond=0)
@@ -143,9 +151,8 @@ class Command(BaseCommand):
 
     def _handle_interval(self, opt_since, opt_until):
         # Process since argument
-        since = self._handle_datelike(opt_since, help=help_since)
+        since = self._handle_datelike(opt_since, help=self.help.get('since'))
 
         # Process until argument
-        until = self._handle_datelike(opt_until, help=help_until)
-
+        until = self._handle_datelike(opt_until, help=self.help.get('until'))
         return since, until
